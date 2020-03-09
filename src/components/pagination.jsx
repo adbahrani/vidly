c7325b8d87e166e28f5c13e1c465076d5f48e823
Adam Bahrani <adbarani2@gmail.com>
2020-03-09 13:44:57 -0400

    Add pagination

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,26 +1,27 @@
 import React, { Component } from "react";
+import _ from "lodash";
 
-class Pagination extends Component {
-  render() {
-    console.log(this.props.count);
-    return (
-      <ul className="pagination">
-        {this.props.pages.map(p => (
-          <li
-            key={p}
-            className={`page-item ${
-              +this.props.selected == p ? "active" : " "
-            }`}
-            onClick={() => this.props.onClick(p)}
-          >
-            <a className="page-link" href="#">
-              {p}
-            </a>
-          </li>
-        ))}
-      </ul>
-    );
-  }
-}
+const Pagination = props => {
+  let count = props.itemCount;
+  let pagesCount = Math.ceil(count / props.pages);
+  if (pagesCount === 1) return null;
+  let pages = _.range(1, pagesCount + 1);
+
+  return (
+    <ul className="pagination">
+      {pages.map(p => (
+        <li
+          key={p}
+          className={`page-item ${+props.selected == p ? "active" : " "}`}
+          onClick={() => props.onClick(p)}
+        >
+          <a className="page-link" href="#">
+            {p}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default Pagination;
